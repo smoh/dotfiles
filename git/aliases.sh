a5fbac50837a57efeaa71fdf04633665f58bad3a
Semyeong Oh <semyeong.oh@gmail.com>
2019-02-28 17:37:37 +0000

    update

---
+++ git/aliases.sh
@@ -0,0 +1,8 @@
+
+alias gs='git status -sb'
+alias gd='git diff'
+alias gb='git branch'
+alias ga='git add'
+alias gc='git commit'
+alias gco='git checkout'
+alias gcd='cd "$(git rev-parse --show-toplevel)"'  # cd to git root
