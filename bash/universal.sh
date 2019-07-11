b5f27cc263eebb3f3d743f17bb0769231227edc4
Semyeong Oh <semyeong.oh@gmail.com>
2019-07-11 21:10:49 +0100

    Move functions

--- bash/universal.sh
+++ bash/universal.sh
@@ -69,3 +69,21 @@ extract () {
     fi
 }
 
+
+# make ssh tunnel to specified port
+ssht () {
+  if [ $# -ne 2 ]; then
+    echo "ssht <port> <destination>"
+    return 1
+  fi
+  ssh -f -N -L $1\:localhost:$1 $2
+}
+
+# list all ports
+lsport () {
+	if [ -x "$(command -v fzf)" ]; then
+		lsof -i -P -n | fzf
+  else
+		lsof -i -P -n
+	fi
+}
